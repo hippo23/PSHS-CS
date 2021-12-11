a3b3b65452073bb34180b858c9543a1b77e6ace2
Simon Magleo <simonmagleo23@gmail.com>
2021-12-11 11:37:26 +0800

    updated with exercise on sorthing algorithms

---
+++ sorting/MAGLEO_simon.py
@@ -0,0 +1,108 @@
+# MAGLEO, Simon Benedict A. 
+# 11 - A
+# LG 4.2 - LG 4.6 Sorting Algorithms
+
+# Exercise Section 1 - Bubble Sort
+def bubble_sort(args):
+    swap_count = -1
+    
+    while(swap_count != 0):
+        swap_count = 0
+
+        for i in range (len(args)-1):
+            try:
+                if args[i] > args[i+1]:
+                    swap_count+=1;
+                    temp = args[i]
+                    args[i] = args[i+1]
+                    args[i+1] = temp
+            except TypeError:
+                if isinstance(args[i], int) == False:
+                    swap_count+=1;
+                    temp = args[i]
+                    args[i] = args[i+1]
+                    args[i+1] = temp
+
+# Exercise Section 2 - Quick Sort
+def quick_sort(args):
+    splitpoint_type = partition_type(args, 0, len(args)-1)
+    left_arr = args[0:splitpoint_type+1]
+    right_arr = args[splitpoint_type+2:len(args)]
+
+    quicksort_helper(left_arr, 0, len(left_arr)-1)
+    quicksort_helper(right_arr, 0, len(right_arr)-1)
+    args = left_arr + right_arr
+
+    return args
+
+def quicksort_helper(args, start, end):
+    if start < end:
+        splitpoint = partition(args, start, end)
+        quicksort_helper(args, start, splitpoint-1)
+        quicksort_helper(args, splitpoint+1, end)
+
+def partition(args,first,last):
+   pivotvalue = args[first] #select pivot value
+   leftmark = first+1 #assign left position marker
+   rightmark = last #assign right position marker
+
+   done = False
+   while not done: #while split point is not foun 
+       while rightmark >= 0 and args[rightmark] <= pivotvalue and rightmark >= leftmark:
+            rightmark = rightmark - 1
+       while leftmark <= len(args)-1 and args[leftmark] >= pivotvalue and leftmark <= rightmark:
+            leftmark = leftmark + 1
+       if rightmark < leftmark:
+            done = True
+       else:
+            temp = args[rightmark]
+            args[rightmark] = args[leftmark]
+            args[leftmark] = temp
+
+   temp = args[first]
+   args[first] = args[rightmark]
+   args[rightmark] = temp
+
+   return rightmark
+
+
+def partition_type(args,first,last):
+   leftmark = first+1 #assign left position marker
+   rightmark = last #assign right position marker
+   swaps = 0
+
+   done = False
+   while not done: #while split point is not foun 
+       while rightmark >= 0 and isinstance(args[rightmark], int) == False and rightmark >= leftmark:
+            rightmark = rightmark - 1
+       while leftmark <= len(args)-1 and isinstance(args[leftmark], int) == True and leftmark <= rightmark:
+            leftmark = leftmark + 1
+       if rightmark < leftmark:
+            done = True
+       elif rightmark >= 0 and leftmark <= len(args)-1:
+            temp = args[rightmark]
+            args[rightmark] = args[leftmark]
+            args[leftmark] = temp
+            swaps+=1
+
+   if swaps > 0:
+       temp = args[first]
+       args[first] = args[rightmark]
+       args[rightmark] = temp
+
+   if isinstance(args[rightmark], int) == True:
+       return rightmark
+   else:
+       return rightmark - 1
+
+
+list1 = [19, 5, 'cat', 'rabbit', 2, 32, 'mice', 'dog', 7]
+list2 = ['traveler', 'paimon', 12, 36, 10, 'venti', 'jean', 29, 34, 75, 'aloy']
+nlist = [54,26,93,17,77,31,44,55,20]
+list2 = quick_sort(list2)
+print(list2)
+# Exercise Section 1 
+
+# Exercise Section 2
+
+# Exercise Section 3
